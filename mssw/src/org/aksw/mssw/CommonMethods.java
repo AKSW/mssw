b7e5b3f90fc0e6834bc6503bbee307862118b32c
Tim Ermilov <yamalight@gmail.com>
2011-02-04 12:46:34 +0100

    some comments add/remove

--- mssw/src/org/aksw/mssw/CommonMethods.java
+++ mssw/src/org/aksw/mssw/CommonMethods.java
@@ -4,6 +4,9 @@ import android.content.ContentResolver;
 
 public class CommonMethods {
 
+	/**
+	 * checks if the MswCore is installed
+	 */
 	public static boolean checkForTripleProvider(ContentResolver cr) {
 		if (cr.acquireContentProviderClient(
 				Constants.TRIPLE_AUTHORITY) == null) {
