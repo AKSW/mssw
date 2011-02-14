4a8bf491ebb6b778cf4f25e5e74316cee816afe5
Natanael Arndt <Natanael Arndt@localhost>
2011-02-14 01:24:44 +0100

    melting both Constants.java-files

--- mssw/src/org/aksw/mssw/triplestore/ModelManager.java
+++ mssw/src/org/aksw/mssw/triplestore/ModelManager.java
@@ -21,6 +21,7 @@ import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLSocketFactory;
 
+import org.aksw.mssw.Constants;
 import org.aksw.mssw.foafssl.TrustManagerFactory;
 
 import android.content.Context;
