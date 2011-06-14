d1e72551d2186fcbf1a4eaa6197d463dfe70bcfd
Natanael Arndt <Natanael Arndt@localhost>
2011-06-14 00:41:00 -0400

    adding UpdateManager that it doesn't get lost (i think this revision is brocken)

---
+++ mssw/src/org/aksw/mssw/UpdaterManager.java
@@ -0,0 +1,36 @@
+package org.aksw.mssw;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+public class UpdaterManager {
+	private Context context;
+	
+	public UpdaterManager (Context contextIn) {
+		context = contextIn;
+	}
+	
+	public boolean needUpdate() {
+
+		SharedPreferences sharedPreferences = PreferenceManager
+				.getDefaultSharedPreferences(context);
+		
+		int version = sharedPreferences.getInt("version", -1);
+		
+		int versionInstalled = 0;
+		
+		if (version < versionInstalled) {
+			return true;
+		}
+		return false;
+	}
+	
+	public void update() {
+		
+	}
+	
+	private void _updateRules() {
+		
+	}
+}
