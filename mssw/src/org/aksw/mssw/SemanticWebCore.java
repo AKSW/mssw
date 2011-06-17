b1a017488c8cf92f95b159b228286102ed341464
Natanael Arndt <Natanael Arndt@localhost>
2011-06-16 22:37:38 -0400

    some minor changes for Update

--- mssw/src/org/aksw/mssw/SemanticWebCore.java
+++ mssw/src/org/aksw/mssw/SemanticWebCore.java
@@ -5,6 +5,13 @@ import org.aksw.mssw.update.UpdateChecker;
 import android.app.Application;
 import android.util.Log;
 
+/**
+ * This Class is the main class, which is always instantiate when the App is running.
+ * So we can do basic stuff like checking if the local configuration is up to date and fits to the installed version.
+ * 
+ * @author natanael
+ *
+ */
 public class SemanticWebCore extends Application {
 
 	private static String TAG = "SemanticWebCore Application";
@@ -19,10 +26,12 @@ public class SemanticWebCore extends Application {
 		if (!updateChecker.isConsistent()) {
 			updateChecker.configure();
 			/**
-			 *  TODO: what to do it not consistent and the configuration fails?
+			 *  TODO: what to do if not consistent and the configuration fails?
 			 *  in this case the UpdateChecker should start a configuration activity
 			 *  like the FirstRun wizard.
 			 *  Is it good to start an Activity even if the TripleProvider is called?
+			 *  Maybe the TripleProvider should throw an exception if the system is not configured.
+			 *  And than we should give the possibility to run the configuration.
 			 */
 		}
 		super.onCreate();
--- mssw/src/org/aksw/mssw/update/WebIdCheck.java
+++ mssw/src/org/aksw/mssw/update/WebIdCheck.java
@@ -1,24 +1,37 @@
 package org.aksw.mssw.update;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 
+/**
+ * This update check check if a webid is configured
+ * 
+ * @author natanael
+ *
+ */
 public class WebIdCheck extends UpdateCheck {
 
+	Context context;
+	
 	@Override
-	public void setContext(Context context) {
-		// TODO Auto-generated method stub
-		
+	public void setContext(Context contextIn) {
+		context = contextIn;
 	}
 
 	@Override
 	public boolean isConsistent() {
-		// TODO Auto-generated method stub
+		SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(context);
+		String webid = pref.getString("me", null);
+		if (webid != null) {
+			return true;
+		}
 		return false;
 	}
 
 	@Override
 	public void configure() throws MswUpdateException {
-		// TODO Auto-generated method stub
+		// TODO show a first run activity to give the user the possibility to configure a webid
 		
 	}
 
